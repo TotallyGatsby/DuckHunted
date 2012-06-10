66a589f69b9ec3f57a1b3a07efc24d7eb15e5fa0
Phillip Spiess <github@phillipspiess.com>
2012-06-10 10:05:42 -0700

    The dog now laughs at you if he is close enough to scent you.

--- Assets/Scripts/TheDog.js
+++ Assets/Scripts/TheDog.js
@@ -9,14 +9,21 @@ var zigTimer:float;
 @System.NonSerialized
 var isZigging = false;
 
+
+// How long before the dog changes directions
 var maxZigTime:float = 25;
 var minZigTime:float = 5;
 
-var speed:float 			= 2;
-var maxSpeed:float 			= 10;
+var speed:float 			= 2; // Dog's current/starting speed
+var maxSpeed:float 			= 10; // Dog's maximum speed
 var huntStrength:float 		= 5; 	// How quickly the dog turns to the player
 var huntAcceleration:float 	= .25; 	// How quickly the hound speeds up
 
+// TODO: Maybe this is a property of ducks? So bigger ducks are easier to shoot, but aren't
+// as alluring to the dog?
+var duckScentRadius:float = 20; // How far away the dog can sense ducks
+
+var playerScentRadius:float = 10; // How far away the dog can sense the player's exact location for a charge!
 
 function Start () {
 	player = GameObject.Find("Player");
@@ -56,4 +63,17 @@ function Update () {
 	if (speed > maxSpeed){
 		speed = maxSpeed;
 	}
+	
+	if (Vector3.Distance(transform.position, player.transform.position) < playerScentRadius &&
+		!audio.isPlaying){
+		audio.Play(0);
+	}
+}
+
+function OnDrawGizmos(){
+	Gizmos.color = Color.blue;
+	Gizmos.DrawWireSphere(transform.position, duckScentRadius);
+	
+	Gizmos.color = Color.yellow;
+	Gizmos.DrawWireSphere(transform.position, playerScentRadius);
 }
