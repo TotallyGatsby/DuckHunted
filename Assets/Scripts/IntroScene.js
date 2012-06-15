9ebb00c718cae7547f58f763f521974615b48b8e
Svampson <Svampson@Sweden>
2012-06-15 16:28:10 +0200

    Worked on #3 some more, still need to hook stuff up

--- Assets/Scripts/IntroScene.js
+++ Assets/Scripts/IntroScene.js
@@ -1,21 +1,14 @@
 #pragma strict
-
+var customSkin : GUISkin;
 var letters:GameObject[];
 var targets:Vector3[];
-var fadeins:GameObject[];
+private var guiAlpha = 0.0;
+private var guiDelay = 2.0;
 
 function Start () {
 	iTween.CameraFadeAdd();
 	iTween.CameraFadeFrom({"amount":1, "time":0});
 	iTween.CameraFadeTo({"amount":0, "time":1});
-	
-	// Set the alphas to 0 on our fadein text
-	for(var obj in fadeins){
-		iTween.FadeTo(obj, {
-			"alpha":0,
-			"time": 0
-		});
-	}
 	
 	for (var i = 0; i < letters.length; i ++){
 		iTween.MoveTo(letters[i], {"position":targets[i], 
@@ -24,13 +17,21 @@ function Start () {
 									"oncomplete":"SoftPunch", 
 									"oncompletetarget":letters[i]});
 	}
+}
+
+function OnGUI() 
+{
+	GUI.skin = customSkin;
+	GUI.color.a = guiAlpha;
 	
-	for(i =0; i<fadeins.length; i++){
-		iTween.FadeTo(fadeins[i], {
-			"alpha":1,
-			"time": 1,
-			"delay":1.4 + i*.2
-		});
-	}
+	guiDelay -= Time.deltaTime;
+	if (guiDelay <= 0)
+		guiAlpha = Mathf.Lerp(guiAlpha, 1, Time.deltaTime*1.5);	
 	
+	if (GUI.Button(Rect(Screen.width-210, Screen.height - 180, 200, 50),"Start Game"))
+		Debug.Log("Clicked Start Game");
+	if (GUI.Button(Rect(Screen.width-210, Screen.height - 120, 200, 50),"Options"))
+		Debug.Log("Clicked Options");
+	if (GUI.Button(Rect(Screen.width-210, Screen.height - 60, 200, 50),"Leaderboards"))
+		Debug.Log("Clicked Leaderboards");
 }
