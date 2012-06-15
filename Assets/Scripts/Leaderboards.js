55a0c53fa961e4d1398ab8d4bd47993666638fa7
Patrik Riström <Svampson@Sweden>
2012-06-15 21:45:41 +0200

    Leaderboards script readded
    (the script didn't get trough last time for some reason!)

---
+++ Assets/Scripts/Leaderboards.js
@@ -0,0 +1,39 @@
+#pragma strict
+var customSkin : GUISkin;
+var sensitivitySliderValue = 10.0;
+
+private var guiAlpha = 1;
+private var guiDelay = 2.0;
+
+function Start () 
+{
+
+}
+
+function OnEnable () 
+{
+	Global.hsController.getScores();
+	Global.options.gameObject.active = false;
+}
+
+function Update () {
+
+}
+
+function OnGUI () 
+{
+	GUI.skin = customSkin;
+	GUI.color.a = guiAlpha;
+	GUI.depth = 11;
+	
+	guiDelay -= Time.deltaTime;
+	if (guiDelay <= 0)
+		guiAlpha = Mathf.Lerp(guiAlpha, 1, Time.deltaTime*1.5);	
+	
+	GUI.Label(Rect (Screen.width/2-128, Screen.height/2-264, 256, 500), "Leaderboards");
+	GUI.Label(Rect (Screen.width/2-228, Screen.height/2-232, 248, 500), Global.hsNameText);
+	GUI.Label(Rect (Screen.width/2+20, Screen.height/2-232, 224, 500), Global.hsScoreText);
+		
+	if (GUI.Button(Rect(Screen.width/2-100, Screen.height/2+180, 200, 50),"Done"))
+		transform.active = false;
+}
